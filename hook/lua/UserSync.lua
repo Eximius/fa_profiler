cb59bf4fc55359b57e2962eb814ae0640d866d92
Eximius <eximius@gmx.com>
2015-05-09 17:49:48 +0300

    ForkThread the gorram SendReport

--- hook/lua/UserSync.lua
+++ hook/lua/UserSync.lua
@@ -2,6 +2,8 @@ local baseOnSync = OnSync
 function OnSync()
 	baseOnSync()
 	if Sync.profilerReport then
-		import('/mods/profiler/lua/profiler.lua').SendReport(Sync.profilerReport)
+		ForkThread(
+			import('/mods/profiler/lua/profiler.lua').SendReport, Sync.profilerReport
+		)
 	end
 end

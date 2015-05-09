da61837634cfd081904a16c99f462f88ce87b730
Eximius <eximius@gmx.com>
2015-05-09 15:02:17 +0300

    Sublime oops with file folder change

--- hook/lua/ui/game/gamemain.lua
+++ hook/lua/ui/game/gamemain.lua
@@ -1,9 +1,6 @@
 local baseOnFirstUpdate = OnFirstUpdate
 function OnFirstUpdate()
 	baseOnFirstUpdate()
-	if not CheatsEnabled() then
-		WARN('You have to have cheats enabled to use the profiler.')
-	else
-		ConExecute("IN_BindKey Ctrl-P SimLua import('/mods/profiler/lua/profiler.lua').Toggle()")
-	end
+	ConExecute("IN_BindKey Ctrl-P SimLua import('/mods/profiler/lua/profiler.lua').Toggle()")
+	ConExecute("IN_BindKey Ctrl-R UI_Lua RestartSession()")
 end
--- hook/lua/ui/gamemain.lua
+++
@@ -1,5 +0,0 @@
-local baseOnFirstUpdate = OnFirstUpdate
-function OnFirstUpdate()
-	baseOnFirstUpdate()
-	ConExecute("IN_BindKey Ctrl-P SimLua import('/mods/profiler/lua/profiler.lua').Toggle()")
-end

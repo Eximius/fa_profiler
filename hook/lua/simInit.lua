f863ffb931d052c62172ca9cfbc1f1f6e667ec75
Eximius <eximius@gmx.com>
2015-05-10 17:11:04 +0100

    Don't start the profiler at game start.

--- hook/lua/simInit.lua
+++ hook/lua/simInit.lua
@@ -1,6 +1,6 @@
-local baseBeginSession = BeginSession
-function BeginSession()
-	baseBeginSession()
-	local Profiler = import('/mods/profiler/lua/profiler.lua')
-	Profiler.Start()
-end
+--local baseBeginSession = BeginSession
+--function BeginSession()
+--	baseBeginSession()
+--	local Profiler = import('/mods/profiler/lua/profiler.lua')
+--	Profiler.Start()
+--end
